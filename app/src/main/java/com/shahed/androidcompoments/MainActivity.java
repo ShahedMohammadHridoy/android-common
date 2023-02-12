ca28f82c8707519836f978277a212df1db785849
Shahed <shahed_hridoy@outlook.com>
2023-02-12 19:37:06 +0600

    TextView

--- app/src/main/java/com/shahed/androidcompoments/MainActivity.java
+++ app/src/main/java/com/shahed/androidcompoments/MainActivity.java
@@ -2,13 +2,31 @@ package com.shahed.androidcompoments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button textViewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewButton = findViewById(R.id.text_view_button);
+        textViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/shahed/androidcompoments/TextViewActivity.java
@@ -0,0 +1,17 @@
+package com.shahed.androidcompoments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class TextViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+}
