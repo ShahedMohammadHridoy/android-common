48ca1a2565fe7d94272ef349a8c83030ee1e910b
Shahed <shahed_hridoy@outlook.com>
2023-06-20 13:07:23 +0600

    spinner

--- app/src/main/java/com/shahed/androidcomponents/RootContainersActivity.java
+++ app/src/main/java/com/shahed/androidcomponents/RootContainersActivity.java
@@ -2,13 +2,28 @@ package com.shahed.androidcomponents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RootContainersActivity extends AppCompatActivity {
+
+    Button buttonSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_root_containers);
+
+
+//        Initialization
+        buttonSpinner = findViewById(R.id.button_spinner);
+
+//        On Click Listener
+        buttonSpinner.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SpinnerActivity.class);
+            startActivity(intent);
+        });
     }
 }
